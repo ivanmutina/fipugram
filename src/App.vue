94b87f7b5b397780e1540ca9e668095bff912a32
Ivan <43207364+ivanmutina@users.noreply.github.com>
2022-07-25 02:15:26 +0200

    Commit
    Added navbar, grid, InstagramCard component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,32 @@
 <template>
   <div id="app">
-    <nav id="nav" class="navbar navbar-light bg-light">
-      <a class="navbar-brand" href="#">
-        <img src="@/assets/fipu_logo.png" height="40" class="d-inline-block align-text-top" loading="lazy" />
-      </a>
-
-      <router-link to="/">Home</router-link> | 
-      <router-link to="/login">Pijava</router-link> |
-      <router-link to="/signup">Registracija</router-link>
+    <nav class="navbar navbar-expand-lg">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          <img src="@/assets/fipu_logo.png" height="40" class="d-inline-block align-text-top" alt="" loading="lazy" />
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <router-link to="/" class="nav-link">Home</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/login" class="nav-link">Pijava</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/signup" class="nav-link">Registracija</router-link>
+            </li>
+          </ul>
+          <form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+          </form>
+        </div>
+      </div>
     </nav>
-    
+
     <div class="container">
       <router-view />
     </div>
